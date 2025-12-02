b00ba9ed425571aaa43afb8cc20790935cd6b032
gazi04 <gazmendhalili2016@gmail.com>
2025-12-02 10:53:00 +0100

    :recycle: Change parent class from EntityDefinition to EntityTranslationDefinition

--- custom/plugins/ProductBundle/src/Core/Content/Product_bundle_translation/Product_bundle_translationDefinition.php
+++ custom/plugins/ProductBundle/src/Core/Content/Product_bundle_translation/Product_bundle_translationDefinition.php
@@ -2,40 +2,40 @@
 
 namespace ProductBundle\Core\Content\Product_bundle_translation;
 
-use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
+use ProductBundle\Core\Content\Product_bundle\Product_bundleDefinition;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityTranslationDefinition;
 use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
-use Shopware\Core\Framework\DataAbstractionLayer\Field\BoolField;
-use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\PrimaryKey;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
-use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\StringField;
 
-class Product_bundle_translationDefinition extends EntityDefinition
+class Product_bundle_translationDefinition extends EntityTranslationDefinition
 {
-    public const ENTITY_NAME = 'product_bundle_translation';
+  public const ENTITY_NAME = 'product_bundle_translation';
 
-    public function getEntityName(): string
-    {
-        return self::ENTITY_NAME;
-    }
+  public function getEntityName(): string
+  {
+    return self::ENTITY_NAME;
+  }
 
-    public function getEntityClass(): string
-    {
-        return Product_bundle_translationEntity::class;
-    }
+  public function getParentDefinitionClass(): string
+  {
+    return Product_bundleDefinition::class;
+  }
 
-    public function getCollectionClass(): string
-    {
-        return Product_bundle_translationCollection::class;
-    }
+  public function getEntityClass(): string
+  {
+    return Product_bundle_translationEntity::class;
+  }
 
-    protected function defineFields(): FieldCollection
-    {
-        return new FieldCollection([
-            (new IdField('id', 'id'))->addFlags(new Required(), new PrimaryKey()),
-            (new StringField('name', 'name')),
-            (new StringField('description', 'description')),
-            (new BoolField('active', 'active'))
-        ]);
-    }
+  public function getCollectionClass(): string
+  {
+    return Product_bundle_translationCollection::class;
+  }
+
+  protected function defineFields(): FieldCollection
+  {
+    return new FieldCollection([
+      (new StringField('title', 'title'))->addFlags(new Required()),
+    ]);
+  }
 }
